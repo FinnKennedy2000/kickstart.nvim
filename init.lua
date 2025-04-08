bd735023b26378c08644b860ca2718c56f138acc
Finn Kennedy <fpkm2000@gmail.com>
2025-04-08 14:53:16 +0100

    fix: php formatting

--- init.lua
+++ init.lua
@@ -756,9 +756,6 @@ require('lazy').setup({
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
@@ -771,12 +768,15 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
         php = { 'php-cs-fixer' },
+      },
+      formatters = {
+        ['php-cs-fixer'] = {
+          command = 'php-cs-fixer',
+          args = { 'fix', '--quiet', '--using-cache=no', '--stdin-path', '$FILENAME', '-' },
+          stdin = true,
+        },
       },
     },
   },
